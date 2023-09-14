6d0dc7b961f0093692cddebc5e65e7cec2e8570c
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-14 15:51:34 -0300

    renaming sql file

---
+++ __sql/1to1.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS adresses (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(150) NOT NULL,
+    user_id INTEGER UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+SELECT * FROM adresses AS a 
+JOIN users u ON a.user_id = u.id;
+
+CREATE TABLE IF NOT EXISTS adresses (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(150) NOT NULL,
+    user_id INTEGER UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICTED
+);
+
+CREATE TABLE IF NOT EXISTS adresses (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(150) NOT NULL,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS adresses (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(150) NOT NULL,
+    user_id INTEGER DEFAULT 1,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET DEFAULT
+);
